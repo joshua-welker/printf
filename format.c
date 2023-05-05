74fcbc800634386a713b8cc8b1fcf3a9028505c8
joshua-welker <joshua.welker@stud.thga.de>
2023-05-05 23:35:23 +0200

    format.c hinzugefügt, aus Termingründen nicht durch den Praktikumspartner

---
+++ format.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int alter = 26; 
+	float gewicht = 69.65165;
+	char batch = 'J';
+	char * name = "Joshua";
+
+	printf("Mein alter ist %d\n", alter);
+	printf("Ich wiege %.2f kg \n ", gewicht);
+	printf("Mein Name beginnt mit einem %c\n", batch);
+	printf("Mein Name ist %s\n", name);
+
+	return 0;
+
+}
