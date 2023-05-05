666438368cd6ca1e59305f13dafec171ef547e4e
joshua-welker <joshua.welker@stud.thga.de>
2023-05-05 23:45:05 +0200

    ansi\_color.c auf branch ansicolor hinzugefügt, das die Farben der ascii art verändert ausgibt

---
+++ ansi_color.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main (){
+        printf("\033[37m __    __  \n");
+        printf("\033[34m/ \\\\..// \\ \n");
+        printf("\033[35m  ( oo )   \n");
+        printf("\033[34m   \\__/    \n");
+        return 0;
+}
